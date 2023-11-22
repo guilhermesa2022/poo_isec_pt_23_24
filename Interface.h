8e2b7fb2367ed62a3fd5eb97caabf9e0bca89660
Guilherme2s <117845571+guilhermesa2022@users.noreply.github.com>
2023-11-22 12:49:59 +0000

    wwwe

--- Interface.h
+++ Interface.h
@@ -2,6 +2,9 @@
 #define PROJECTCASA_INTERFACE_H
 
 #include "Comando.h"
+#include "Terminal.h"
+#include "Habitacao.h"
+using namespace term;
 using namespace std;
 
 
@@ -11,5 +14,11 @@ string getCmd();
 
 // gui
 void UI2();
+void atualizar_zonas_UI(int linhas, int colunas,int tamzona, Habitacao &H, Window ***wzonas);
+void inicializar(Terminal& t, int *dim);
+Window ini_cmd_UI(int *dim);
+void ini_cor(Terminal& t);
+void atualizar_cmd(Window &Wcmd);
+
 
 #endif //PROJECTCASA_INTERFACE_H
--- dadospas/main.cpp
+++ dadospas/main.cpp
@@ -1,53 +1,33 @@
-#include "Interface.h"
-#include "Habitacao.h"
-#include "utils.h"
+#include <iostream>
 #include <sstream>
+#include <iomanip>
+#include "Zona.h"
+#include "Terminal.h"
+#include "Habitacao.h"
+#include "Interface.h"
 
-void UI() {
-    string cmd;
-    int res;
-    // * ... aqui.
-    while (1) {
-        cmd = "";
-        res = -1;
-        Comando c; // <- Exprimentar por este comando ... *
+using namespace term;
 
-        cmd = getCmd();
-        res = c.validaCmd(cmd);
-        switch (res) {
-            case 0:
-                cout << c.descricao();
-                cout << '\n';
-                if (c.validaStx()) {
-                    c.execCmd();
-                    cout << "\nSintaxe valida";
-                } else {
-                    cout << "\nSintaxe invalida";
-                }
-                break;
-            case 1:
-                wcout << L"Comando nao encontrado";
-                break;
-            case 2:
-                cout << "Faltam argumentos";
-                break;
-            case 3:
-                cout << "Demasiados argumentos";
-                break;
-            default:
-                break;
-        }
-        // Comando c é destruido aqui
+
+
+void inicializar(Terminal& t, int *dim){
+    dim[0] = t.getNumCols();
+    dim[1] = t.getNumRows();
+    t.clear();
+}
+Window ini_cmd_UI(int *dim){
+    return Window(0, dim[1] - 3, dim[0], 3);
+}
+void ini_cor(Terminal& t){
+    for(int i=1; i<20; i++) {
+        t.init_color(i, i, 0);
     }
 }
-
-string getCmd() {
-    string cmd;
-    cin.clear();
-    cout << "\n\nComando -> ";
-    getline(cin, cmd);
-    return cmd;
+void atualizar_cmd(Window &Wcmd){
+    Wcmd.clear();
+    Wcmd << set_color(3) << "comando: ";
 }
+
 
 void atualizar_zonas_UI(int linhas, int colunas,int tamzona, Habitacao &H, Window ***wzonas){
     for (int i = 0; i < linhas; ++i) {
@@ -73,29 +53,31 @@ void atualizar_zonas_UI(int linhas, int colunas,int tamzona, Habitacao &H, Windo
     }
 }
 
-void UI2() {
+
+
+int main() {
     int ee;
     int dim[2];
 
     cout << "teste = 1 " << "simular = 2 ";
-    cin >> ee;
+    cin >> ee ;
 
-    if (ee == 2) {
+    if(ee == 2){
         Terminal &t = Terminal::instance();
         inicializar(t, dim);
         ini_cor(t);
-        Window Wcmd = ini_cmd_UI(dim);
+        Window Wcmd =  ini_cmd_UI(dim);
         atualizar_cmd(Wcmd);
 
-        vector<Window *> wzona;
+        vector<Window*> wzona;
 
 
         string numeroDeZOnas = "quantas zonas 2x2 ate 4x4";
-        t << move_to(dim[0] / 2 - (numeroDeZOnas.size() / 2), dim[1] / 2 - 4) << numeroDeZOnas;
+        t << move_to(dim[0] / 2 - (numeroDeZOnas.size()/2), dim[1] / 2 - 4) << numeroDeZOnas;
         Wcmd >> numeroDeZOnas;
 
-        int p = (int) numeroDeZOnas[0] - 48;
-        int f = (int) numeroDeZOnas[2] - 48;
+        int p = (int)numeroDeZOnas[0] - 48;
+        int f = (int)numeroDeZOnas[2] - 48;
 
         Habitacao He(p, f);
         /*for (int i = 0; i < p; ++i) {
@@ -103,14 +85,14 @@ void UI2() {
                 He.add_Zona(i,j);
             }
         }*/
-        He.add_Zona(0, 0);
-        He.add_Zona(p - 1, f - 1);
-        He.add_Zona(p - 1, 0);
+        He.add_Zona(0,0);
+        He.add_Zona(p-1,f-1);
+        He.add_Zona(p-1,0);
         Window ***wzona2;
-        wzona2 = new Window **[p];
+        wzona2 = new Window**[p];
 
         for (int i = 0; i < p; ++i) {
-            wzona2[i] = new Window *[f];
+            wzona2[i] = new Window*[f];
         }
 
         for (int i = 0; i < p; ++i) {
@@ -119,23 +101,27 @@ void UI2() {
             }
         }
 
-        int ztam = (dim[1] - 3) / 4;
+        int ztam = (dim[1] -3) / 4;
 
 
-        atualizar_zonas_UI(p, f, ztam, He, wzona2);
+        atualizar_zonas_UI(p, f,ztam,He, wzona2);
         /*for(int i = 0; i < p; i++)
             for(int j = 0; j < f; j++){
                 wzona.push_back(new Window(ztam * 2 * j, i * ztam , ztam * 2, ztam));
             }*/
         atualizar_cmd(Wcmd);
-        Window wdados = Window(f * (ztam * 2), 0, dim[0] - f * (ztam * 2), ztam * 4);
+        Window wdados =  Window(f * (ztam * 2) , 0, dim[0] - f*(ztam * 2), ztam*4);
         wdados << set_color(3) << f;
         Wcmd >> numeroDeZOnas;
-        He.add_Zona(p - 2, 0);
+        He.add_Zona(p-2,0);
 
-        atualizar_zonas_UI(p, f, ztam, He, wzona2);
+        atualizar_zonas_UI(p, f,ztam,He, wzona2);
 
         Wcmd >> numeroDeZOnas;
+
+
+
+
     }else{
         for (int i = 0; i < 3; ++i) {
             cout << i << " " << endl;
@@ -169,26 +155,7 @@ void UI2() {
         Habitacao He(2, 3);
 
         std::cout << "Hello, World!" << std::endl;
-
     }
-}
-void inicializar(Terminal& t, int *dim){
-    dim[0] = t.getNumCols();
-    dim[1] = t.getNumRows();
-    t.clear();
-}
-Window ini_cmd_UI(int *dim){
-    return Window(0, dim[1] - 3, dim[0], 3);
-}
-void ini_cor(Terminal& t){
-    for(int i=1; i<20; i++) {
-        t.init_color(i, i, 0);
-    }
-}
-void atualizar_cmd(Window &Wcmd){
-    Wcmd.clear();
-    Wcmd << set_color(3) << "comando: ";
-}
 
-
-
+    return 0;
+}
